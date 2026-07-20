9b4103be922371099ec8335db5cfbaf7ae86cce5
MarzNet <vv2trfsx@gmail.com>
2026-07-20 20:24:10 +0330

    Create Dashboard.jsx

---
+++ dashboard/src/pages/Dashboard.jsx
@@ -0,0 +1,41 @@
+import Card from "../components/Card";
+
+
+function Dashboard(){
+
+
+return (
+
+<div>
+
+<h1>
+Dashboard
+</h1>
+
+
+<Card
+title="Users"
+value="0"
+/>
+
+
+<Card
+title="Servers"
+value="0"
+/>
+
+
+<Card
+title="Active Services"
+value="0"
+/>
+
+
+</div>
+
+)
+
+}
+
+
+export default Dashboard;
