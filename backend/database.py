ad21533d096da2139b36a67d8966fcb279d3d480
MarzNet <vv2trfsx@gmail.com>
2026-07-20 19:18:36 +0330

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 DATABASE_URL = (
@@ -21,3 +21,6 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+
+Base = declarative_base()
