d8a9773fb4d4876d773f0fc6652370eff1b0559b
MarzNet <vv2trfsx@gmail.com>
2026-07-20 20:32:16 +0330

    Create Subscriptions.jsx

---
+++ dashboard/src/pages/Subscriptions.jsx
@@ -0,0 +1,24 @@
+function Subscriptions(){
+
+return (
+
+<div>
+
+<h1>
+📦 Subscription Management
+</h1>
+
+
+<p>
+Manage plans and user subscriptions
+</p>
+
+
+</div>
+
+)
+
+}
+
+
+export default Subscriptions;
