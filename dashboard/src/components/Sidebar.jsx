3cdf1039a76184757bb7c3c055296c13a733b3b4
MarzNet <vv2trfsx@gmail.com>
2026-07-20 20:20:50 +0330

    Create Sidebar.jsx

---
+++ dashboard/src/components/Sidebar.jsx
@@ -0,0 +1,44 @@
+function Sidebar(){
+
+return (
+
+<div>
+
+<h2>
+🚀 V2MarzNet
+</h2>
+
+
+<ul>
+
+<li>
+Dashboard
+</li>
+
+<li>
+Users
+</li>
+
+<li>
+Servers
+</li>
+
+<li>
+Subscriptions
+</li>
+
+<li>
+Settings
+</li>
+
+</ul>
+
+
+</div>
+
+)
+
+}
+
+
+export default Sidebar;
