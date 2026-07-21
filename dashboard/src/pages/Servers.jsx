7d65154316961381bdcb5618f79732bf59cdfa1f
MarzNet <vv2trfsx@gmail.com>
2026-07-21 10:09:01 +0330

    Update Servers.jsx

--- dashboard/src/pages/Servers.jsx
+++ dashboard/src/pages/Servers.jsx
@@ -1,17 +1,55 @@
+import {useEffect,useState} from "react";
+import api from "../api";
+
+
 function Servers(){
+
+const [servers,setServers]=useState([]);
+
+
+useEffect(()=>{
+
+api.get("/servers")
+.then(res=>{
+
+setServers(
+res.data.servers
+)
+
+})
+
+},[])
+
+
 
 return (
 
 <div>
 
 <h1>
-🌍 Servers Management
+🌍 Servers
 </h1>
 
 
+{
+
+servers.length===0 ?
+
 <p>
-Manage VPN servers and locations
+No servers
 </p>
+
+:
+
+servers.map((server,index)=>(
+
+<p key={index}>
+{server.name}
+</p>
+
+))
+
+}
 
 
 </div>
