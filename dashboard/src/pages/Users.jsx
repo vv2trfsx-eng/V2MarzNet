19ffe5c8ebbed7fa482499a67fa19ceb7601027c
MarzNet <vv2trfsx@gmail.com>
2026-07-21 10:06:34 +0330

    Update Users.jsx

--- dashboard/src/pages/Users.jsx
+++ dashboard/src/pages/Users.jsx
@@ -1,16 +1,57 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+
+
 function Users(){
+
+const [users,setUsers] = useState([]);
+
+
+useEffect(()=>{
+
+api.get("/users")
+.then(response=>{
+
+setUsers(
+response.data.users
+);
+
+});
+
+
+},[]);
+
+
 
 return (
 
 <div>
 
 <h1>
-👥 Users Management
+👥 Users
 </h1>
 
+
+{
+
+users.length === 0 ?
+
 <p>
-Manage V2MarzNet users
+No users found
 </p>
+
+:
+
+users.map((user,index)=>(
+
+<p key={index}>
+{user.username}
+</p>
+
+))
+
+}
+
 
 </div>
 
